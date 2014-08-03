c2bf8873e2b85d0bb7d1c9e6cfd9f146ed6134a7
Josh Reisner <josh@joshreisner.com>
2014-08-03 07:54:33 -0700

    hopefully improving chronological order

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -67,16 +67,17 @@ $(document).ready(function(){
 	    $container.isotope('reLayout');
 	});
 
+	//add in elements when they're loaded
 	$('article.loading').each(function(){
-		
-		var $this = $(this).imagesLoaded(function(){
+		var $this = $(this);
+		$this.imagesLoaded(function(){
 			$this.removeClass("loading");
 			$container.isotope("appended", $this);
 		});
-
 	});
 
-	$("#filter a").click(function(e) {
+	//handle filter menu click
+	$("#filter").on("click", "a", function(e) {
 
 		if ($(this).parent().hasClass("all")) {
 			filter = "*";
@@ -131,15 +132,3 @@ $(document).ready(function(){
 
 	});
 });
-
-/*
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-80350-2']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-*/
--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -63,5 +63,18 @@
             @endforeach
         </section>
         <script src="/assets/js/main.min.js"></script>
+        @if (App::environment() != 'local')
+        <script>
+	        var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-80350-2']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
+		@endif
     </body>
 </html>
